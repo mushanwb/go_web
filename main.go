46604f3a4afc542b979e6bc57b64997064e6a9cf
wangbo <1216476406@qq.com>
2020-12-11 11:06:58 +0800

    [自动加载】 安装 air
    设置国内网站：
    `go env -w  GOPROXY=https://goproxy.cn`
    
    安装 air
    `GO111MODULE=on  go get -u github.com/cosmtrek/air`
    
    检查安装成功
    `air -v`
    
    运行
    `air`
    
    每次修改文件会自动启动 main.go
    产生一个 tmp 文件夹,忽略即可

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	url := r.URL.Path
 	switch url {
 	case "/":
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+		fmt.Fprint(w, "<h1>Hello, 欢迎来到 Golang！</h1>")
 	case "/about":
 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
