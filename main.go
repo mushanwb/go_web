124f7034be6605ed3a895900cf412da8526f07ae
wangbo <1216476406@qq.com>
2020-12-11 10:58:33 +0800

    [路由匹配] 根据 switch 匹配每个路由,展示不同数据

--- main.go
+++ main.go
@@ -6,8 +6,18 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
-	fmt.Fprint(w, "请求路径："+r.URL.Path)
+	url := r.URL.Path
+	switch url {
+	case "/":
+		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	case "/about":
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	default:
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
+	}
+
 }
 func main() {
 	http.HandleFunc("/", handlerFunc)
