c905efb41ea5fdd3da187b8c835029adccf85af0
wangbo <1216476406@qq.com>
2020-12-11 10:39:14 +0800

    [路由匹配]任意路径 "/"
    "/" 代表匹配任意路由,例如：
    输入:
    localhost:3000/
    localhost:3000/about
    localhost:3000/no-where
    
    输出: 请求路径:
    /
    /about
    /no-where

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	fmt.Fprint(w, "请求路径："+r.URL.Path)
 }
 func main() {
 	http.HandleFunc("/", handlerFunc)
