620f081246830df40559e526327af33589a61560
wangbo <1216476406@qq.com>
2020-12-12 10:08:27 +0800

    设置 HandleFunc, 将路由由 HandleFunc 函数管理,不在通过 switch 判断

--- main.go
+++ main.go
@@ -5,23 +5,27 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	url := r.URL.Path
 	switch url {
 	case "/":
 		fmt.Fprint(w, "<h1>Hello, 欢迎来到 Golang！</h1>")
-	case "/about":
-		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 	default:
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
 			"<p>如有疑惑，请联系我们。</p>")
 	}
-
 }
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/about", aboutHandler)
 	http.ListenAndServe(":3000", nil)
 }
