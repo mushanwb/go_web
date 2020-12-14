ff72ea257eda0fb70a2f9e4611ce8c455debc22c
wangbo <1216476406@qq.com>
2020-12-14 15:15:21 +0800

    创建 博文表单
    使用 html 编写博文表单，并且将提交的数据发送给另外一个路由
    
    由于在另外一个函数中使用了 router 变量，在 go 里面函数变量是不共享的
    因此需要将 router 放到包级别，移出 main ，包级别的变量声明不能再使用 := 赋值
    而需要使用 var

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"strings"
 )
 
+// 使用 精准匹配 的 gorilla/mux
+var router = mux.NewRouter()
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
 }
@@ -24,6 +27,27 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "访问文章列表")
+}
+
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
+	html := `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>创建文章 —— 我的技术博客</title>
+</head>
+<body>
+    <form action="%s" method="post">
+        <p><input type="text" name="title"></p>
+        <p><textarea name="body" cols="30" rows="10"></textarea></p>
+        <p><button type="submit">提交</button></p>
+    </form>
+</body>
+</html>
+`
+	// 将提交的数据发送给 articles.store 这个路由
+	storeURL, _ := router.Get("articles.store").URL()
+	fmt.Fprintf(w, html, storeURL)
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
@@ -55,8 +79,6 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 }
 
 func main() {
-	// 使用 精准匹配 的 gorilla/mux
-	router := mux.NewRouter()
 
 	// 后面的 Name 属性是给路由命名,和 laravel 路由的 name 属性差不多
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
@@ -68,6 +90,7 @@ func main() {
 	// 同名的路由,根据请求的方式不同，选择进入不同的函数
 	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 
 	// 自定义 404 页面
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
