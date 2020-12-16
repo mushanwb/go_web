f9f752426162f8fc0494a2c38ab8a316bd958df6
wangbo <1216476406@qq.com>
2020-12-16 12:27:06 +0800

    将文章字段的验证代码提取出来

--- main.go
+++ main.go
@@ -135,21 +135,7 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.PostFormValue("title")
 	body := r.PostFormValue("body")
 
-	errors := make(map[string]string)
-
-	// 验证标题
-	if title == "" {
-		errors["title"] = "标题不能为空"
-	} else if len(title) < 3 || len(title) > 40 {
-		errors["title"] = "标题长度介于 3~40"
-	}
-
-	// 验证内容
-	if body == "" {
-		errors["body"] = "内容不能为空"
-	} else if len(body) < 10 {
-		errors["body"] = "内容长度需大于或等于 10 个字节"
-	}
+	errors := validateArticleFormData(title, body)
 
 	// 检查是否有错误
 	if len(errors) == 0 {
@@ -289,21 +275,7 @@ func articlesUpdateHandler(w http.ResponseWriter, r *http.Request) {
 		title := r.PostFormValue("title")
 		body := r.PostFormValue("body")
 
-		errors := make(map[string]string)
-
-		// 验证标题
-		if title == "" {
-			errors["title"] = "标题不能为空"
-		} else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(title) > 40 {
-			errors["title"] = "标题长度需介于 3-40"
-		}
-
-		// 验证内容
-		if body == "" {
-			errors["body"] = "内容不能为空"
-		} else if utf8.RuneCountInString(body) < 10 {
-			errors["body"] = "内容长度需大于或等于 10 个字节"
-		}
+		errors := validateArticleFormData(title, body)
 
 		if len(errors) == 0 {
 			// 4.2 表单验证通过，更新数据
@@ -340,9 +312,26 @@ func articlesUpdateHandler(w http.ResponseWriter, r *http.Request) {
 
 			tmpl.Execute(w, data)
 		}
+	}
+}
 
+func validateArticleFormData(title string, body string) map[string]string {
+	errors := make(map[string]string)
+	// 验证标题
+	if title == "" {
+		errors["title"] = "标题不能为空"
+	} else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(title) > 40 {
+		errors["title"] = "标题长度需介于 3-40"
 	}
 
+	// 验证内容
+	if body == "" {
+		errors["body"] = "内容不能为空"
+	} else if utf8.RuneCountInString(body) < 10 {
+		errors["body"] = "内容长度需大于或等于 10 个字节"
+	}
+
+	return errors
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
