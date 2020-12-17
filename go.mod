5200a3271626a26b41f2ed12a11ba6aac8f3e82e
wangbo <1216476406@qq.com>
2020-12-17 15:52:53 +0800

    第一个测试
    安装测试依赖：
    github.com/stretchr/testify
    
    测试 home 接口

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
-module github.com/mushanwb/go_web
+module go_web
 
 go 1.14
 
 require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
+	github.com/stretchr/testify v1.6.1
 )
---
+++ tests/home_test.go
@@ -0,0 +1,23 @@
+package tests
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestHome(t *testing.T) {
+	baseURL := "http://localhost:3000"
+
+	// 1. 请求 —— 模拟用户访问浏览器
+	var (
+		resp *http.Response
+		err  error
+	)
+
+	resp, err = http.Get(baseURL + "/")
+
+	// 2. 检测 —— 是否无错误且 200
+	assert.NoError(t, err, "有错误发生")
+	assert.Equal(t, 200, resp.StatusCode, "应返回状态码 200")
+}
