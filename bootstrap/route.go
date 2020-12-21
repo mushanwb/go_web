a13e8b093c4324e77221b31b5a6812103ce03ce0
wangbo <1216476406@qq.com>
2020-12-21 17:39:09 +0800

    [重构] 将初始化路由的操作放入 bootstrap 包中

---
+++ bootstrap/route.go
@@ -0,0 +1,12 @@
+package bootstrap
+
+import (
+	"github.com/gorilla/mux"
+	"go_web/route"
+)
+
+func SetupRoute() *mux.Router {
+	router := mux.NewRouter()
+	route.RegisterWebRoutes(router)
+	return router
+}
