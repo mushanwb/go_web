a077b1721a9cbe26a9eaedf231b2b46fd9a52a86
wangbo <1216476406@qq.com>
2020-12-22 16:38:30 +0800

    [重构] 删除多余的代码

--- main.go
+++ main.go
@@ -1,110 +1,11 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
 	_ "github.com/go-sql-driver/mysql"
 	"go_web/bootstrap"
-	"go_web/database"
-	"go_web/pkg/types"
 	"net/http"
 	"strings"
-	"unicode/utf8"
 )
-
-var db *sql.DB
-
-// Article  对应一条文章数据
-type Article struct {
-	ID    int64  `json:"id"`
-	Title string `json:"title"`
-	Body  string `json:"body"`
-}
-
-// 返回的 json 数据格式
-type JsonResult struct {
-	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
-}
-
-func ReturnJson(message string, data interface{}) []byte {
-	jsonData, _ := json.Marshal(JsonResult{message, data})
-	return jsonData
-}
-
-func saveArticleToDB(title string, body string) (int64, error) {
-	// 变量初始化
-	var (
-		id   int64
-		err  error
-		rs   sql.Result
-		stmt *sql.Stmt
-	)
-
-	// 1,获取一个 prepare 声明语句
-	stmt, err = db.Prepare("INSERT INTO articles (title, body) VALUES(?,?)")
-	// 例行检查错误
-	if err != nil {
-		return 0, err
-	}
-
-	// 2. 在此函数运行结束后关闭此语句，防止占用 SQL 连接
-	defer stmt.Close()
-
-	// 3. 执行请求，传参进入绑定的内容
-	rs, err = stmt.Exec(title, body)
-	if err != nil {
-		return 0, err
-	}
-
-	// 4. 插入成功的话，会返回自增 ID
-	if id, err = rs.LastInsertId(); id > 0 {
-		return id, nil
-	}
-
-	return 0, err
-}
-
-func getArticleByID(id string) (Article, error) {
-	article := Article{}
-	query := "SELECT * FROM articles WHERE id = ?"
-	err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
-
-	return article, err
-}
-
-func (a Article) Delete() (rowsAffected int64, err error) {
-	rs, err := db.Exec("DELETE FROM articles WHERE id = " + types.Int64ToString(a.ID))
-	if err != nil {
-		return 0, err
-	}
-
-	// 删除成功
-	if n, _ := rs.RowsAffected(); n > 0 {
-		return n, nil
-	}
-
-	return 0, nil
-}
-
-func validateArticleFormData(title string, body string) map[string]string {
-	errors := make(map[string]string)
-	// 验证标题
-	if title == "" {
-		errors["title"] = "标题不能为空"
-	} else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(title) > 40 {
-		errors["title"] = "标题长度需介于 3-40"
-	}
-
-	// 验证内容
-	if body == "" {
-		errors["body"] = "内容不能为空"
-	} else if utf8.RuneCountInString(body) < 10 {
-		errors["body"] = "内容长度需大于或等于 10 个字节"
-	}
-
-	return errors
-}
 
 // 中间件,给每个请求头设置返回头数据格式
 func forceHTMLMiddleware(next http.Handler) http.Handler {
@@ -126,10 +27,6 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 }
 
 func main() {
-
-	// 初始化数据库
-	database.Initialize()
-	db = database.DB
 
 	bootstrap.SetupDB()
 	router := bootstrap.SetupRoute()
