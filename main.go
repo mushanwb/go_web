72a5f58d0b76132b59132b79f9688afed9948231
wangbo <1216476406@qq.com>
2020-12-22 17:00:50 +0800

    [重构] 迁移中间件

--- main.go
+++ main.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	_ "github.com/go-sql-driver/mysql"
+	"go_web/app/http/middlewares"
 	"go_web/bootstrap"
 	"net/http"
 	"strings"
 )
-
-// 中间件,给每个请求头设置返回头数据格式
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 设置返回头的数据格式
-		w.Header().Set("Content-Type", "application/json")
-		// 继续处理请求
-		next.ServeHTTP(w, r)
-	})
-}
 
 func removeTrailingSlash(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +23,7 @@ func main() {
 	router := bootstrap.SetupRoute()
 
 	// 中间件：强制内容类型为 JSON
-	router.Use(forceHTMLMiddleware)
+	router.Use(middlewares.ForceJson)
 
 	// 通过命名路由获取 URL 示例
 	//homeURL, _ := router.Get("home").URL()
