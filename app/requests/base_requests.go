227c8d3a2344f3b49f091131c9c59ca71126e154
wangbo <1216476406@qq.com>
2020-12-25 12:10:03 +0800

    [重构] 将注册和登录的参数校验使用基类里面的方法

--- app/requests/base_requests.go
+++ app/requests/base_requests.go
@@ -1,4 +1,20 @@
 package requests
 
+import "github.com/thedevsaddam/govalidator"
+
 type BaseRequest struct {
+}
+
+func (*BaseRequest) Options(data interface{}, rules map[string][]string, messages map[string][]string) map[string][]string {
+	opts := govalidator.Options{
+		Data:          &data,
+		Rules:         rules,
+		TagIdentifier: "valid", // Struct 标签标识符
+		Messages:      messages,
+	}
+
+	// 4. 开始认证
+	errs := govalidator.New(opts).ValidateStruct()
+
+	return errs
 }
---
+++ app/requests/user_login.go
@@ -0,0 +1,25 @@
+package requests
+
+import "github.com/thedevsaddam/govalidator"
+
+type LoginFromData struct {
+	Email    string `valid:"email"`
+	Password string `valid:"password"`
+}
+
+func ValidateLoginFrom(loginFromData LoginFromData) map[string][]string {
+	baseRequest := BaseRequest{}
+	// 1. 定制认证规则
+	rules := govalidator.MapData{
+		"email":    baseRequest.EmailRules(),
+		"password": baseRequest.EmailRules(),
+	}
+
+	// 2. 定制错误消息
+	messages := govalidator.MapData{
+		"email":    baseRequest.EmailMessages(),
+		"password": baseRequest.PasswordMessages(),
+	}
+
+	return baseRequest.Options(loginFromData, rules, messages)
+}
